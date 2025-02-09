11b86c1c4437395000d79707d1dd9eab740a0b9d
adityakumar-in <adityaakumar.in@gmail.com>
2025-02-10 01:29:25 +0530

    Updated Home Section

--- app/page.js
+++ app/page.js
@@ -6,65 +6,94 @@ import SignUpProfile from '@/components/authentication/SignUpProfile';
 import '@/app/styles/home/home.css';
 import Assessment from '@/components/authentication/Assessment';
 import Statistic from '@/components/Statistic';
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Page = () => {
   const [showSignUpProfile, setShowSignUpProfile] = useState(false);
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState('');
 
-  useEffect(() => {
-    const checkUsername = async () => {
-      if (auth.currentUser) {
-        try {
-          const uid = auth.currentUser.uid;
-          const usernameRef = ref(db, `usernames/${uid}/username`);
-          const snapshot = await get(usernameRef);
-        } catch (error) {
-          console.error('Error checking username:', error);
-        } finally {
-          setLoading(false);
-        }
-      } else {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const checkUsername = async () => {
+  //     if (auth.currentUser) {
+  //       try {
+  //         const uid = auth.currentUser.uid;
+  //         const usernameRef = ref(db, `usernames/${uid}/username`);
+  //         const snapshot = await get(usernameRef);
+  //       } catch (error) {
+  //         console.error('Error checking username:', error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     } else {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        checkUsername();
-        const displayName = user.displayName;
-        const email = user.email;
-        if (displayName) {
-          setUsername(displayName);
-        } else if (email) {
-          const emailName = getCurrentUserEmailName();
-          setUsername(emailName.split('.')[0]);
-        } else {
-          setUsername('null');
-        }
-      } else {
-        setUsername('null');
-        setLoading(false);
-        setShowSignUpProfile(false);
-      }
-    });
+  //   const unsubscribe = auth.onAuthStateChanged((user) => {
+  //     if (user) {
+  //       checkUsername();
+  //       const displayName = user.displayName;
+  //       const email = user.email;
+  //       if (displayName) {
+  //         setUsername(displayName);
+  //       } else if (email) {
+  //         const emailName = getCurrentUserEmailName();
+  //         setUsername(emailName.split('.')[0]);
+  //       } else {
+  //         setUsername('null');
+  //       }
+  //     } else {
+  //       setUsername('null');
+  //       setLoading(false);
+  //       setShowSignUpProfile(false);
+  //     }
+  //   });
 
-    return () => {
-      unsubscribe();
-      // Cleanup any remaining containers on unmount
-      const container = document.getElementById('signup-profile-container');
-      if (container) {
-        container.remove();
-      }
-    };
-  }, []);
+  //   return () => {
+  //     unsubscribe();
+  //     // Cleanup any remaining containers on unmount
+  //     const container = document.getElementById('signup-profile-container');
+  //     if (container) {
+  //       container.remove();
+  //     }
+  //   };
+  // }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <main>
-      <Statistic />
+      <nav className='navbar'>
+        <Link href="/" className="site-name">
+          Medix<span> AI</span>
+        </Link>
+        <div className="web-login">Login</div>
+      </nav>
+
+      <div className="hero-section">
+        <div className="hero-section-content">
+          <h1 className='hero-section-title'>Our Healthcare Solutions Meet Every Need</h1>
+          <p className='hero-section-description'>Unlock your best self with AI-powered health insights! Get a detailed health report and personalized tips to boost your well-being effortlessly.</p>
+          <div className='hero-section-button'>Check Now</div>
+        </div>
+
+        <div className="hero-section-img">
+          <div className="hero-section-image">
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+            <div className="line-3">
+              <div className="line-3-1"></div>
+              <div className="line-3-2"></div>
+            </div>
+            <div className="line-1"></div>
+          </div>
+        </div>
+      </div>
+
     </main>
   );
 }
